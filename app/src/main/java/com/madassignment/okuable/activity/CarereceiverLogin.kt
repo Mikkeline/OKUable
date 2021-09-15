f91b67b6680dc55ea8ef7de3fabc7c0b5cc987e7
YenKwanNee <cindyyen12@gmail.com>
2021-09-15 22:43:34 +0800

    Initial commit

---
+++ app/src/main/java/com/madassignment/okuable/activity/CarereceiverLogin.kt
@@ -0,0 +1,138 @@
+package com.madassignment.okuable.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.*
+import com.google.firebase.firestore.auth.User
+import com.madassignment.okuable.R
+import com.okuable.okuable.data.Users
+import kotlinx.android.synthetic.main.activity_carereceiver_login.*
+import kotlinx.android.synthetic.main.activity_carereceiver_register.*
+
+class CarereceiverLogin : AppCompatActivity() {
+
+    lateinit var mDatabase: DatabaseReference
+    var user = FirebaseAuth.getInstance().currentUser
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_carereceiver_login)
+
+
+        val backRegister: TextView = findViewById(R.id.tvBackreg)
+        backRegister.setOnClickListener() {
+
+            val intent = Intent(this, CarereceiverRegister::class.java)
+            startActivity(intent)
+
+        }
+
+        val loginButton: Button = findViewById(R.id.btnRegister_receiver)
+        loginButton.setOnClickListener() {
+
+            when {
+                TextUtils.isEmpty(
+                    email_edittext_receiver_login.text.toString().trim { it <= ' ' }) -> {
+                    Toast.makeText(
+                        this@CarereceiverLogin,
+                        "Please enter email.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                }
+                TextUtils.isEmpty(
+                    password_edittext_receiver_login.text.toString().trim { it <= ' ' }) -> {
+                    Toast.makeText(
+                        this@CarereceiverLogin,
+                        "Please enter password.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                else -> {
+                    val receiverEmail: String =
+                        email_edittext_receiver_login.text.toString().trim { it <= ' ' }
+                    val receiverPassword: String =
+                        password_edittext_receiver_login.text.toString().trim { it <= ' ' }
+
+                    FirebaseAuth.getInstance()
+                        .signInWithEmailAndPassword(receiverEmail, receiverPassword)
+                        .addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+
+                                val uid = user!!.uid
+                                val firebaseUser: FirebaseUser = task.result!!.user!!
+
+                                mDatabase = FirebaseDatabase.getInstance().getReference("Users")
+                                mDatabase.child(uid).child("userType").get().addOnSuccessListener {
+
+                                    var userType = it.value.toString()
+
+                                    if (userType == "Care Receiver") {
+                                        Toast.makeText(
+                                            this@CarereceiverLogin,
+                                            "You are logging as Care Receiver!",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                        val intent = Intent(
+                                            this@CarereceiverLogin,
+                                            MainActivity2::class.java
+                                        )
+                                        intent.flags =
+                                            Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                        intent.putExtra("user_id", firebaseUser.uid)
+                                        intent.putExtra("receiverEmail_id", receiverEmail)
+                                        startActivity(intent)
+                                    } else if (userType == "Care Giver") {
+                                        Toast.makeText(
+                                            this@CarereceiverLogin,
+                                            "You are logging as Care Giver!",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                        val intent = Intent(
+                                            this@CarereceiverLogin,
+                                            MainActivity::class.java
+                                        )
+                                        intent.flags =
+                                            Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                        intent.putExtra("user_id", firebaseUser.uid)
+                                        intent.putExtra("receiverEmail_id", receiverEmail)
+                                        startActivity(intent)
+                                    } else if (userType == "Admin") {
+                                        Toast.makeText(
+                                            this@CarereceiverLogin,
+                                            "You are logging in as Admin!",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                        val intent = Intent(
+                                            this@CarereceiverLogin,
+                                            MainActivity3::class.java
+                                        )
+                                        intent.flags =
+                                            Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                        intent.putExtra("user_id", firebaseUser.uid)
+                                        intent.putExtra("receiverEmail_id", receiverEmail)
+                                        startActivity(intent)
+                                    }
+                                }
+                            } else {
+                                Toast.makeText(
+                                    this@CarereceiverLogin,
+                                    task.exception!!.message.toString(),
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        }
+                }
+            }
+        }
+    }
+}
+
---
+++ app/src/main/java/com/madassignment/okuable/activity/CarereceiverRegister.kt
@@ -0,0 +1,175 @@
+package com.madassignment.okuable.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_carereceiver_register.*
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.madassignment.okuable.R
+import android.content.ContentValues.TAG
+import android.text.TextUtils
+import android.widget.*
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.madassignment.okuable.fragment.*
+
+class CarereceiverRegister : AppCompatActivity() {
+    val mAuth = FirebaseAuth.getInstance()
+    lateinit var mDatabase : DatabaseReference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_carereceiver_register)
+
+
+        mDatabase = FirebaseDatabase.getInstance().getReference("Users")
+
+        val haveAccount: TextView = findViewById(R.id.tvHaveaccount)
+        haveAccount.setOnClickListener() {
+            Log.d("CarereceiverRegister", "Try to show login activity")
+
+            val intent = Intent(this, CarereceiverLogin::class.java)
+            startActivity(intent)
+
+        }
+
+        val registerButton: Button = findViewById(R.id.btnRegister_receiver)
+        registerButton.setOnClickListener() {
+
+            when {
+                TextUtils.isEmpty(
+                    email_edittext_receiver_reg.text.toString().trim { it <= ' ' }) -> {
+                    Toast.makeText(
+                        this@CarereceiverRegister,
+                        "Please enter email.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                }
+                TextUtils.isEmpty(
+                    password_edittext_receiver_reg.text.toString().trim { it <= ' ' }) -> {
+                    Toast.makeText(
+                        this@CarereceiverRegister,
+                        "Please enter password.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                TextUtils.isEmpty(
+                    username_edittext_receiver_reg.text.toString().trim { it <= ' ' }) -> {
+                    Toast.makeText(
+                        this@CarereceiverRegister,
+                        "Please enter username.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                else -> {
+
+                    val receiverEmail: String =
+                        email_edittext_receiver_reg.text.toString().trim { it <= ' ' }
+                    val receiverPassword: String =
+                        password_edittext_receiver_reg.text.toString().trim { it <= ' ' }
+                    val receiverUsername: String =
+                        username_edittext_receiver_reg.text.toString().trim { it <= ' ' }
+                    val Selection: RadioGroup = radioGroup1
+                    var userType: String = tvUserType.text.toString().trim { it <= ' '}
+
+                    val cr: RadioButton = findViewById(R.id.radio0)
+                    val cg: RadioButton = findViewById(R.id.radio1)
+                    val ad: RadioButton = findViewById(R.id.radio2)
+                    if (cr.isChecked){
+                        userType = "Care Receiver"
+                    }
+                    else if (cg.isChecked){
+                        userType = "Care Giver"
+                    }
+                    else {
+                        userType = "Admin"
+                    }
+
+
+                    FirebaseAuth.getInstance()
+                        .createUserWithEmailAndPassword(receiverEmail, receiverPassword)
+                        .addOnCompleteListener(
+                            OnCompleteListener<AuthResult> { task ->
+                                if (task.isSuccessful) {
+                                    val firebaseUser: FirebaseUser = task.result!!.user!!
+
+                                    val user = mAuth.currentUser
+                                    val uid = user!!.uid
+
+                                    mDatabase.child(uid).child("uid").setValue(uid)
+                                    mDatabase.child(uid).child("username").setValue(receiverUsername)
+                                    mDatabase.child(uid).child("email").setValue(receiverEmail)
+                                    mDatabase.child(uid).child("password").setValue(receiverPassword)
+                                    mDatabase.child(uid).child("userType").setValue(userType)
+
+
+
+                                    Toast.makeText(
+                                        this@CarereceiverRegister,
+                                        "You are registered successfully!",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+
+
+
+                                    if (userType == "Care Receiver"){
+                                        val intent = Intent(
+                                            this@CarereceiverRegister,
+                                            MainActivity2::class.java
+                                        )
+                                        intent.flags =
+                                            Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                        intent.putExtra("user_id", firebaseUser.uid)
+                                        intent.putExtra("receiverEmail_id", receiverEmail)
+                                        startActivity(intent)
+                                    }
+                                    else if (userType == "Care Giver"){
+                                        val intent = Intent(
+                                            this@CarereceiverRegister,
+                                            MainActivity::class.java
+                                        )
+                                        intent.flags =
+                                            Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                        intent.putExtra("user_id", firebaseUser.uid)
+                                        intent.putExtra("receiverEmail_id", receiverEmail)
+                                        startActivity(intent)
+                                    }
+                                    else if (userType == "Admin"){
+                                        val intent = Intent(
+                                            this@CarereceiverRegister,
+                                            MainActivity3::class.java
+                                        )
+                                        intent.flags =
+                                            Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                        intent.putExtra("user_id", firebaseUser.uid)
+                                        intent.putExtra("receiverEmail_id", receiverEmail)
+                                        startActivity(intent)
+                                    }
+
+
+                                } else {
+                                    Toast.makeText(
+                                        this@CarereceiverRegister,
+                                        task.exception!!.message.toString(),
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                }
+
+                            })
+
+
+                }
+            }
+        }
+
+    }
+}
